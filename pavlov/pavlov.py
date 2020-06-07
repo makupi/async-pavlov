68d59f3371e3c804ab7509a817b6171b786e31e0
makubob <makupi@icloud.com>
2020-06-07 19:39:06 +0200

    Keeps connections alive, auto-reconnects on closed connection

--- pavlov/pavlov.py
+++ pavlov/pavlov.py
@@ -15,18 +15,50 @@ class PavlovRCON:
         self.timeout = timeout
         self.reader, self.writer = None, None
 
-    async def _connect(self):
-        self.reader, self.writer = await asyncio.wait_for(
-            asyncio.open_connection(self.ip, self.port), self.timeout
-        )
+    async def open(self):
+        if not self.is_connected():
+            await self._connect()
 
-    async def _disconnect(self):
-        self.writer.close()
-        await self.writer.wait_closed()
+    async def close(self):
+        if self.is_connected():
+            await self._disconnect()
+
+    def is_connected(self):
+        if self.writer and not self.writer.is_closing():
+            return True
+        return False
+
+    async def send(self, command, wait_response=True, auto_close=False):
+        if not self.is_connected():
+            await self._connect()
+        await self._send(command)
+        data = None
+        if wait_response:
+            data = await self._recv()
+        if auto_close:
+            await self._disconnect()
+        return data
 
     async def _send(self, data):
         self.writer.write(data.encode())
         await self.writer.drain()
+
+    async def _auth(self):
+        await self._send(self.password)
+        data = await self._recv()
+        if "Authenticated=1" not in data:
+            raise InvalidPassword
+
+    async def _connect(self):
+        self.reader, self.writer = await asyncio.wait_for(
+            asyncio.open_connection(self.ip, self.port), self.timeout
+        )
+        await self._auth()
+
+    async def _disconnect(self):
+        if self.writer:
+            self.writer.close()
+            await self.writer.wait_closed()
 
     async def _recv(self):
         try:
@@ -40,19 +72,3 @@ class PavlovRCON:
         except Exception as ex:
             print(f"EXCEPTION <_recv>: {ex}")
             return None
-
-    async def _auth(self):
-        await self._send(self.password)
-        data = await self._recv()
-        if "Authenticated=1" not in data:
-            raise InvalidPassword
-
-    async def send(self, command, wait_response=True):
-        await self._connect()
-        await self._auth()
-        await self._send(command)
-        data = None
-        if wait_response:
-            data = await self._recv()
-        await self._disconnect()
-        return data
